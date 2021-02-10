0a701f4ecb4f33c92808a02e07d6d4e956b12565
rosf73 <47631768+rosf73@users.noreply.github.com>
2021-02-10 21:19:31 +0900

    레이아웃 배치 및 스크롤뷰 테스트

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
+  const daily_todo = "엄인태 하루일과 화이팅 ^^";
+  const daily_plans = [
+    {
+      test: "엄인태1 하루계획 화이팅 ^^"
+    },
+    {
+      test: "엄인태2 하루계획 화이팅 ^^"
+    },
+    {
+      test: "엄인태3 하루계획 화이팅 ^^"
+    },
+  ];
+
+  const renderElements = (plan_list) => {
+    return plan_list.map(elem => {
+      return (
+        <View>
+          <Text>{elem.test}</Text>
+        </View>
+      );
+    });
+  }
+
+  const day_elements = renderElements(daily_plans);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.daily}>
+        <Text>{daily_todo}</Text>
+      </View>
+      <ScrollView style={styles.week}>
+        {day_elements}
+      </ScrollView>
+      <View style={styles.detail}>
+        <Text>엄인태 세부일과 화이팅 ^^</Text>
+      </View>
     </View>
   );
 }
@@ -17,5 +48,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: '2%'
   },
+  daily: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: '2%',
+    width: '95%'
+  },
+  week: {
+    flex: 5,
+    marginBottom: '2%',
+    width: '100%'
+  },
+  detail: {
+    flex: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: '2%',
+    width: '95%'
+  }
 });
