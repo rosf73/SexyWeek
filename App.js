d9185e46f21753ac530979a03d62c8d65a035526
rosf73 <47631768+rosf73@users.noreply.github.com>
2021-02-10 23:01:53 +0900

    리스트 데이터 적용 테스트

--- App.js
+++ App.js
@@ -1,42 +1,78 @@
 import React from 'react';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { SimpleLineIcons as Icon } from '@expo/vector-icons';
 
 export default function App() {
-  const daily_todo = "엄인태 하루일과 화이팅 ^^";
+  const daily_todo = "아침점심저녁 먹기";
   const daily_plans = [
     {
-      test: "엄인태1 하루계획 화이팅 ^^"
+      daily: "엄인태1 하루계획 화이팅 ^^",
+      detail: [
+        "엄인태11 세부일과 화이팅 ^^",
+        "엄인태12 세부일과 화이팅 ^^",
+        "엄인태13 세부일과 화이팅 ^^"
+      ]
     },
     {
-      test: "엄인태2 하루계획 화이팅 ^^"
+      daily: "엄인태2 하루계획 화이팅 ^^",
+      detail: [
+        "엄인태21 세부일과 화이팅 ^^",
+        "엄인태22 세부일과 화이팅 ^^",
+        "엄인태23 세부일과 화이팅 ^^"
+      ]
     },
     {
-      test: "엄인태3 하루계획 화이팅 ^^"
+      daily: "엄인태3 하루계획 화이팅 ^^",
+      detail: [
+        "엄인태31 세부일과 화이팅 ^^",
+        "엄인태32 세부일과 화이팅 ^^",
+        "엄인태33 세부일과 화이팅 ^^"
+      ]
     },
   ];
 
-  const renderElements = (plan_list) => {
+  const renderDayElements = (plan_list) => {
     return plan_list.map(elem => {
       return (
         <View>
-          <Text>{elem.test}</Text>
+          <Text>{elem.daily}</Text>
+        </View>
+      );
+    });
+  }
+  const renderDetailElements = (detail_plan_list) => {
+    return detail_plan_list.map(elem => {
+      return (
+        <View>
+          <Text>{elem}</Text>
         </View>
       );
     });
   }
 
-  const day_elements = renderElements(daily_plans);
+  const day_elements = renderDayElements(daily_plans);
+  const detail_elements = renderDetailElements(daily_plans[0].detail);
 
   return (
     <View style={styles.container}>
       <View style={styles.daily}>
-        <Text>{daily_todo}</Text>
+        <Icon name="pencil" size={20} color="green"/>
+        <Text style={styles.daily_text}>{daily_todo}</Text>
       </View>
-      <ScrollView style={styles.week}>
-        {day_elements}
-      </ScrollView>
+      <View style={styles.week}>
+        <ScrollView
+          contentContainerStyle={styles.week_scroll}
+          horizontal={true}
+        >
+          {day_elements}
+        </ScrollView>
+      </View>
       <View style={styles.detail}>
-        <Text>엄인태 세부일과 화이팅 ^^</Text>
+        <ScrollView
+          contentContainerStyle={styles.detail_scroll}
+        >
+          {detail_elements}
+        </ScrollView>
       </View>
     </View>
   );
@@ -45,27 +81,42 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: Platform.OS === `ios` ? 0 : Expo.Constants.statusBarHeight,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
     padding: '2%'
   },
   daily: {
-    flex: 1,
-    justifyContent: 'center',
+    flex: 0.6,
+    backgroundColor: 'orange',
+    alignItems: 'center',
     marginBottom: '2%',
-    width: '95%'
+    width: '100%',
+    borderRadius: 10,
+    paddingHorizontal: '3%',
+    flexDirection: 'row'
+  },
+  daily_text: {
+    marginLeft: '2%'
   },
   week: {
-    flex: 5,
+    flex: 4,
     marginBottom: '2%',
     width: '100%'
+  },
+  week_scroll: {
+    flexGrow: 1,
+    alignItems: 'center',
+    flexDirection: 'row'
   },
   detail: {
     flex: 8,
     alignItems: 'center',
     justifyContent: 'center',
-    marginBottom: '2%',
+    marginBottom: '5%',
     width: '95%'
+  },
+  detail_scroll: {
   }
 });
---
+++ Components/Day.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Day() {
+  return (
+    <View style={styles.container}>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '30%',
+    height: '100%'
+  }
+});
